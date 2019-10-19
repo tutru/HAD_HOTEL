3a862172d133793b91ee2e2666ab732633f30dc1
Admin <ledien2803@gmail.com>
2019-10-19 21:06:47 +0700

    Add RoomStatus

---
+++ src/main/java/com/had/hotelmanagement/controller/RoomStatusController.java
@@ -0,0 +1,81 @@
+package com.had.hotelmanagement.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.had.hotelmanagement.entity.RoomStatus;
+import com.had.hotelmanagement.service.RoomStatusService;
+
+@Controller
+@RequestMapping(value = "")
+public class RoomStatusController {
+	@Autowired
+	private RoomStatusService roomStatusService;
+
+	@RequestMapping(value = { "/roomstatus-list" }, method = RequestMethod.GET)
+	public String listrole(Model model) {
+		model.addAttribute("listRoomStatus", roomStatusService.findAll());
+		return "roomstatus-list";
+	}
+
+	@RequestMapping("/roomstatus-save")
+	public String insertRoomStatus(Model model) {
+		model.addAttribute("roomstatus", new RoomStatus());
+		return "roomstatus-save";
+	}
+
+	@RequestMapping("/roomstatus-view/{roomstatusid}")
+	public String viewRoomStatus(@PathVariable int roomstatusid, Model model) {
+		RoomStatus roomstatus = roomStatusService.findByIdRoomStatus(roomstatusid);
+		model.addAttribute("roomstatus", roomstatus);
+		return "roomstatus-view";
+	}
+
+	@RequestMapping("/roomstatus-update/{roomstatusid}")
+	public String updateRoomStatus(@PathVariable int roomstatusid, Model model) {
+		RoomStatus roomstatus = roomStatusService.findByIdRoomStatus(roomstatusid);
+		model.addAttribute("roomstatus", roomstatus);
+		return "roomstatus-update";
+	}
+
+	@RequestMapping(value = "/saveRoomStatus", method = RequestMethod.POST)
+	public String doSaveRoomStatus(ModelMap model, HttpServletRequest request,
+			@ModelAttribute("roomstatus") RoomStatus roomstatus) {
+		roomStatusService.save(roomstatus);
+		model.addAttribute("listRoomStatus", roomStatusService.findAll());
+		return "roomstatus-list";
+	}
+
+	@RequestMapping("/updateRoomStatus")
+	public String doUpdateRoomStatus(Model model, @ModelAttribute("roomstatus") RoomStatus roomstatus) {
+		roomStatusService.update(roomstatus);
+		model.addAttribute("listRoomStatus", roomStatusService.findAll());
+		return "roomstatus-list";
+
+	}
+
+	@RequestMapping("/roomstatus-delete/{roomstatusid}")
+	public String doDeleteRoomStatus(@PathVariable int roomstatusid, Model model) {
+		roomStatusService.delete(roomstatusid);
+		model.addAttribute("listRoomStatus", roomStatusService.findAll());
+		return "roomstatus-list";
+	}
+
+	@RequestMapping(value = "/roomstatus-search")
+	public String search(String roomstatusname, Model model) {
+		List<RoomStatus> roomstatus =roomStatusService.searchRoomStatus(roomstatusname);
+		model.addAttribute("search", roomstatus);
+		return "roomstatus-search";
+	}
+
+}
