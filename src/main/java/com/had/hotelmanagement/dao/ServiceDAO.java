3ff3811ed09a9a883503ca3c9d8a2cecd06698bb
ngocdu99 <54654962+ngocdu99@users.noreply.github.com>
2019-10-27 21:32:09 +0700

    du đã commit các bạn vừa lòng chưa
    du đã commit các bạn vừa lòng chưa

---
+++ src/main/java/com/had/hotelmanagement/dao/ServiceDAO.java
@@ -0,0 +1,53 @@
+package com.had.hotelmanagement.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.had.hotelmanagement.entity.Customer;
+import com.had.hotelmanagement.entity.CustomerMapper;
+import com.had.hotelmanagement.entity.EmployeeEntity;
+import com.had.hotelmanagement.entity.EmployeeMapper;
+import com.had.hotelmanagement.entity.ServiceEntity;
+import com.had.hotelmanagement.entity.ServiceMapper;
+
+@Repository
+@Transactional
+public class ServiceDAO {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	public void save(ServiceEntity service) {
+		String sql = "INSERT INTO service (servicename,servicedesc) VALUES (?,?)";
+		jdbcTemplate.update(sql, service.getServicename(),
+				service.getServicedesc());
+	}
+
+	public void delete(int serviceid) {
+		String sql = "DELETE FROM service WHERE serviceid = " + serviceid;
+		jdbcTemplate.update(sql);
+	}
+	
+	public void update(ServiceEntity service) {
+		String sql = "UPDATE service SET  servicename = ?, servicedesc=?  WHERE serviceid = ? ";
+		jdbcTemplate.update(sql, service.getServicename(),service.getServicedesc(), service.getServiceid());
+	}
+
+	public ServiceEntity findById(int id) {
+		String sql = "SELECT * FROM service WHERE serviceid = ?";
+		return jdbcTemplate.queryForObject(sql, new ServiceMapper(), id);
+	}
+
+	public List<ServiceEntity> findAll() {
+		String sql = "SELECT * FROM service";
+		return jdbcTemplate.query(sql, new ServiceMapper());
+	}
+	
+	public List<ServiceEntity>findService(String name){
+		String sql = "SELECT * FROM service WHERE service_name LIKE '%"+name+"%'";
+		return jdbcTemplate.query(sql, new ServiceMapper());
+	}
+
+}
