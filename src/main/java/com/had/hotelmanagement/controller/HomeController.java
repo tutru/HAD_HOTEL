375ae596cc20102727f06c5595ad2f3ad29ddccf
changeanh <trantheanh459@gmail.com>
2019-11-06 20:26:18 +0700

    ok

--- src/main/java/com/had/hotelmanagement/controller/HomeController.java
+++ src/main/java/com/had/hotelmanagement/controller/HomeController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
    
-	@RequestMapping(value={"/","/index"})
+	@RequestMapping(value={"/index"})
 	public String index() {
 		return "index";
+	}
+	@RequestMapping(value={"/admin"})
+	public String admin() {
+		return "admin";
 	}
 
 }
