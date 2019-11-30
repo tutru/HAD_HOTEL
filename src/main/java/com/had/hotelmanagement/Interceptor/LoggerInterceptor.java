d9ed11c1de06381883d0412322b8d380a0af731e
changeanh <trantheanh459@gmail.com>
2019-11-30 11:08:29 +0700

    the anh

---
+++ src/main/java/com/had/hotelmanagement/Interceptor/LoggerInterceptor.java
@@ -0,0 +1,29 @@
+package com.had.hotelmanagement.Interceptor;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+public class LoggerInterceptor extends HandlerInterceptorAdapter {
+
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		System.out.println("đang vào");
+
+		HttpSession session = request.getSession();
+		boolean isLogined = session.getAttribute("check") != null ? (Boolean) session.getAttribute("check") : false;
+
+		if (!isLogined) {
+			response.sendRedirect("login");
+			return false;
+
+		}
+
+		return true;
+
+	}
+
+	
+}
